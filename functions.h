42bc51ffaf614e3c2cf5e6fa26b3f6bc336cb46f
Darnoker <73139334+Darnoker@users.noreply.github.com>
2021-01-08 17:05:34 +0100

    Header and C files

---
+++ functions.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define rows 20
+#define columns 60
+
+typedef struct
+{
+    int x;
+    int y;
+    int head;
+    int tail;
+    int Gx;
+
+} Tsnake;
+
+typedef struct
+{
+    int x;
+    int y;
+    int number;
+
+} Tfood;
+
+
+void gameScreen(int field[][columns], int *, int *, Tsnake *);
+void snakeInit(Tsnake *, int field[][columns], int *);
+void resetScreen();
+void foodPrint(Tfood *, int field[][columns]);
+void movement(int *, int *, Tsnake *, int field[][columns], Tfood *, int *, int *, bool *);
+int getch_noblock();
+void tailManager(Tsnake *, int field[][columns]);
+void gameOver(int *, bool *, int *);
